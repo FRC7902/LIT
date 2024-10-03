a46e4df9d5dcb4431f6c256fbaa6ac328345e2a3
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2024-10-03 02:02:58 -0400

    Update light_emitter.py

--- light_emitter.py
+++ light_emitter.py
@@ -1,20 +1,22 @@
 from machine import UART, Pin
 from ws2812 import WS2812
 
-test_uart2 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
-ws = WS2812(machine.Pin(2),20)
+test_uart2 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1)) #declaration of uart connection (uart port, baudrate, tx, rx)
+ws = WS2812(machine.Pin(2),20) #declaration of led strip (pin, # of leds)
 
-def lightstrip(r, g, b):
-    for i in range(0, 19):
-        ws[i] = [r, g, b]
-    ws.write()    
+def lightstrip(r, g, b): #function to change colours on lightstrip
+    for i in range(0, 19): 
+        ws[i] = [r, g, b] #updates all the leds on the rgb strip to one colour, may be changed to have multiple colours
+    ws.write() #turns the specified leds on with the specified colour
     
 
-
+#r8js in a loop so that light values will change if theres a new input value
 while True:
     if test_uart2.any(): #checks if there's data being sent
-        input = test_uart2.read().decode()
         
+        input = test_uart2.read().decode() #stores inputted value
+
+        #test values to see if the pico will update the colour based on the input it receives
         if input == "1":
             lightstrip(255, 0, 0)
         elif input == "2":

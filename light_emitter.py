5e0cf5a252b76da9e5038771e65acad958af2087
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2024-10-03 21:29:30 -0400

    Update light_emitter.py
    added new light modes, allowed transmission of RGB values via UART, lowered baud rate from 9600 to 4800 for transmission reliability, added ability to halt program

--- light_emitter.py
+++ light_emitter.py
@@ -1,32 +1,63 @@
 from machine import UART, Pin
 from ws2812 import WS2812
+import utime
 
-test_uart2 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1)) #declaration of uart connection (uart port, baudrate, tx, rx)
-ws = WS2812(machine.Pin(2),20) #declaration of led strip (pin, # of leds)
+rio = UART(0, baudrate = 4800, tx = Pin(0), rx = Pin(1))
+ws = WS2812(machine.Pin(2),20) #60 leds, to change later
 
-def lightstrip(r, g, b): #function to change colours on lightstrip
-    for i in range(0, 19): 
-        ws[i] = [r, g, b] #updates all the leds on the rgb strip to one colour, may be changed to have multiple colours
-    ws.write() #turns the specified leds on with the specified colour
-    
+#Connect TX to Pin  (1)
+#Connect RX to Pin 1 (2)
+#Connect DATA Pin of WS2812 to Pin 2 (4)
 
-#r8js in a loop so that light values will change if theres a new input value
+def emitStaticColour(r, g, b):
+    for i in range(0, 19):
+        ws[i] = [r, g, b]
+    ws.write()
+
+def flashingLights(r, g, b):
+    emitStaticColour(r, g, b)
+    utime.sleep(0.5)
+    emitStaticColour(0, 0, 0)
+    utime.sleep(0.5)
+
+def emitAlternatingColour(r, g, b, r2, g2, b2):
+    emitStaticColour(r, g, b)
+    utime.sleep(1)
+    emitStaticColour(r2, g2, b2)
+    utime.sleep(1)
+
+emitStaticColour(0, 0, 0)
+
 while True:
-    if test_uart2.any(): #checks if there's data being sent
+    if rio.any(): #checks if there's data being sent
+        input = rio.read().decode().split(",")
+        if input[0] == 'end':
+            emitStaticColour(0, 0, 0)
+            break
         
-        input = test_uart2.read().decode() #stores inputted value
-
-        #test values to see if the pico will update the colour based on the input it receives
-        if input == "1":
-            lightstrip(255, 0, 0)
-        elif input == "2":
-            lightstrip(0, 0, 255)
-        elif input == "3":
-            lightstrip(255, 255, 0)
-        elif input == "4":
-            lightstrip(0, 255, 0)
-        elif input == "5":
-            lightstrip(255, 165, 0)
-        else:
-            pass
+        r, g, b, pattern = int(input[0]), int(input[1]), int(input[2]), int(input[3])
+            
+        if pattern == 0: #static colour mode
+            emitStaticColour(r, g, b)
+            
+        elif pattern == 1: #flashing light mode
+            while True:
+                interrupt = rio.read()
+                if interrupt:
+                    interrupt = interrupt.decode("utf-8")
+                if interrupt == "stop":
+                    break
+                flashingLights(r, g, b)
+        
+        elif pattern == 2: #alternating colour mode
+            while True:
+                interrupt = rio.read()
+                if interrupt:
+                    interrupt = interrupt.decode("utf-8")
+                if interrupt == "stop":
+                    break
+                r2, g2, b2 = int(input[4]), int(input[5]), int(input[6])
+                emitAlternatingColour(r, g, b, r2, g2, b2)
+                
+          
 

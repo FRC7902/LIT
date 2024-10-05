a119067e9bc191d32c60850bd1b985dfbad5f937
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2024-10-04 21:44:42 -0400

    Update light_uartC.ino
    updated flies with issues

--- light_uartC.ino
+++ light_uartC.ino
@@ -5,10 +5,13 @@
 
 #define LED_PIN 2
 #define NUM_LEDS 20
+#define IRQ_PIN 3
 CRGB leds[NUM_LEDS];
 
-const int irq_pin = 3;
-bool irq_received;
+char input;
+bool irq_received = false;
+int i;
+String numberInputted;
 
 int rgb[] = {0, 1, 2, 3, 4, 5, 6};
           //First 0 = Pattern
@@ -20,14 +23,15 @@ void emitStaticColour(int r, int g, int b) {
   FastLED.show();
 }
 
-
 void flashingLights(int r, int g, int b) {
   while (irq_received != true) {
+    Serial.println("aaa");
     emitStaticColour(r, g, b);
     delay(500);
     emitStaticColour(0, 0, 0);
     delay(500);
   }
+  rgb[0] = -5; 
 }
 
 void emitAlternatingColour(int r, int g, int b, int r2, int g2, int b2) {
@@ -37,6 +41,9 @@ void emitAlternatingColour(int r, int g, int b, int r2, int g2, int b2) {
     emitStaticColour(r2, g2, b2);
     delay(1000);
   }
+
+  rgb[0] = -5;
+
 }
 
 void interrupt() {
@@ -48,35 +55,49 @@ void setup() {
   Serial.begin(9600);
   delay(200);
   Serial1.begin(4800, SERIAL_8N1);
-  //Serial.println("SERIAL1_TX: " + String(SERIAL1_TX));
-  //Serial.println("SERIAL1_RX: " + String(SERIAL1_RX));
-  pinMode(irq_pin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(irq_pin), interrupt, CHANGE);
+  Serial.println("SERIAL1_TX: " + String(SERIAL1_TX));
+  Serial.println("SERIAL1_RX: " + String(SERIAL1_RX));
+ // pinMode(IRQ_PIN, INPUT_PULLDOWN);
+  attachInterrupt(digitalPinToInterrupt(IRQ_PIN), interrupt, RISING);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   emitStaticColour(0, 0, 0);
 }
 
 void loop() {
-    
-  for (int i = 0; Serial1.available(); i++) {
-    delay(2);
-    rgb[i] = Serial1.read();
+  while (Serial1.available()) {
+    input = Serial1.read();
+    if (input == 32 || input == 44 || input == 46) {
+      rgb[i] = numberInputted.toInt();
+      i++;
+      numberInputted = "";
+      continue;
+    }
+    numberInputted += input;
   }
 
-  if (rgb[0] == -1) {
+  if (rgb[0] == 0) {
+    emitStaticColour(rgb[1], rgb[2], rgb[3]);
+    i = 0;
+  }
+
+  else if (rgb[0] == 1) {
+    irq_received = false;
+    flashingLights(rgb[1], rgb[2], rgb[3]);
+    i = 0;
+    irq_received = false;
+  }
+
+  else if (rgb[0] == 2) {
+    emitAlternatingColour(rgb[1], rgb[2], rgb[3], rgb[4], rgb[5], rgb[6]);
+    i = 0;
+    irq_received = false;
+  }
+
+  else if (rgb[0] == -1) {
     emitStaticColour(0, 0, 0);
     exit(0);
   }
 
-  else if (rgb[0] == 1) {
-    flashingLights(rgb[1], rgb[2], rgb[3]);
-    irq_received = false;
-    }
-
-  else if (rgb[0] == 2) {
-    emitAlternatingColour(rgb[1], rgb[2], rgb[3], rgb[4], rgb[5], rgb[6]);
-    irq_received = false;
-  }  
 }
 
 

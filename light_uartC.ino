15cda65ecd431f2909e848ff9f7482d1bd08e472
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2024-10-06 21:48:56 -0400

    Update light_uartC.ino
    - added ability to change brightness
    - fixed led lights turning blue on mode 5 while colour was supposed to be white
    - added functions for light modes
    - lowered baud rate to 300

--- light_uartC.ino
+++ light_uartC.ino
@@ -8,9 +8,9 @@
 #define IRQ_PIN 3
 CRGB leds[NUM_LEDS];
 
-char r, g, b, mode, stringy[5];
+char mode, brightness, r, g, b, stringy[5];
 bool irq_flag = false, direction = true, isToBeTurnedOn = true;
-int i;
+int i, bytes_transmitted;
 
 void emitStaticColourAll(int r, int g, int b, int brightness) {
   for (int i = 0; i != 20; i++) {
@@ -24,15 +24,78 @@ void emitStaticColour(int r, int g, int b, int brightness, int ledIndex) {
   FastLED.show(brightness);
 }
 
+void flashingLights(int r, int g, int b, int brightness) {
+  emitStaticColourAll(0, 0, 0, 0);
+  while (irq_flag == false) {
+    emitStaticColourAll(r, g, b, 100);
+    delay(500);
+    emitStaticColourAll(0, 0, 0, 100);
+    delay(500);
+  }
+}
+
+void rainbowLights(int brightness) {
+  i = 0;
+  emitStaticColourAll(0, 0, 0, 0);
+  while (irq_flag == false) {
+    i = (direction == true) ? (i + 1) : (i - 1);
+    emitStaticColourAll(i, 0, 255 - i, 100);
+    delay(30);
+    if (i == 255) {
+      direction = false;
+    }
+    else if (i == 0) {
+      direction = true;
+    }
+  }
+}
+
+void fadingLights(int r, int g, int b, int brightness) {
+  i = 0;
+  emitStaticColourAll(0, 0, 0, 0);
+  while (irq_flag == false) {
+    i = (direction == true) ? (i + 1) : (i - 1);
+    emitStaticColourAll(r, g, b, i);
+    delay(10);
+    if (i == 255) {
+      direction = false;
+    }
+    else if (i == 0) {
+      direction = true;
+    }
+  }
+}
+
+void patternLights(int r, int g, int b, int brightness) {
+  i = 0;
+  emitStaticColourAll(0, 0, 0, 0);      
+  while (irq_flag == false) {
+    if (isToBeTurnedOn == true) {
+      emitStaticColour(r, g, b, 100, i);
+    }
+    else {
+      emitStaticColour(0, 0, 0, 100, i);
+    }
+    delay(100);
+    if (i == 20) {
+      isToBeTurnedOn = !(isToBeTurnedOn);
+      i = 0;
+    }
+    else {
+      i++;
+    }  
+  }
+}
+
 void interrupt() {
   irq_flag = true;
 }
 
 void setup() {
-  delay(2000);
+  delay(2200);
   //Serial.begin(9600); //computer connection
   //delay(200);
-  Serial1.begin(4800, SERIAL_8N1); //pico connection
+  Serial1.begin(300, SERIAL_8N1); //pico connection
   //Serial.println("SERIAL1_TX: " + String(SERIAL1_TX));
   //Serial.println("SERIAL1_RX: " + String(SERIAL1_RX));
   pinMode(IRQ_PIN, INPUT_PULLDOWN);
@@ -42,84 +105,59 @@ void setup() {
 }
 
 void loop() {
-  if (Serial1.available() >= 4) {
+  if (Serial1.available()) {
+    brightness = mode = r = g = b = 0;
+    bytes_transmitted = Serial1.available();
+    //Serial.println(bytes_transmitted);
     irq_flag = false;
-    mode = Serial1.read();
-    r = Serial1.read();
-    g = Serial1.read();
-    b = Serial1.read();
+    brightness = (bytes_transmitted >= 0) ? (Serial1.read()) : (0);
+    mode = (bytes_transmitted >= 1) ? (Serial1.read()) : (0);
+    r = (bytes_transmitted >= 2) ? (Serial1.read()) : (0);
+    g = (bytes_transmitted >= 3) ? (Serial1.read()) : (0);
+    b = (bytes_transmitted >= 4) ? (Serial1.read()) : (0);
 
-    //sprintf(stringy, "%d", mode);
-    //Serial.println(stringy);            
+    delay(5);
 
-    if (mode == 1) {
+    /*
+    sprintf(stringy, "%d", brightness);
+    Serial.println(stringy);    
+    
+    sprintf(stringy, "%d", mode);
+    Serial.println(stringy);      
+
+    sprintf(stringy, "%d", r);
+    Serial.println(stringy);            
+
+    sprintf(stringy, "%d", g);
+    Serial.println(stringy);            
+
+    sprintf(stringy, "%d", b);
+    Serial.println(stringy);            
+    */
+
+    if (brightness == 0) {
       emitStaticColourAll(0, 0, 0, 0);
-      emitStaticColourAll(r, g, b, 100);
+    }
+
+    else if (mode == 1) {
+      emitStaticColourAll(0, 0, 0, 0);
+      emitStaticColourAll(r, g, b, brightness);
     }
 
     else if (mode == 2) {
-      emitStaticColourAll(0, 0, 0, 0);
-      while (irq_flag == false) {
-        emitStaticColourAll(r, g, b, 100);
-        delay(500);
-        emitStaticColourAll(0, 0, 0, 100);
-        delay(500);
-      }
+      flashingLights(r, g, b, brightness);
     }
 
     else if (mode == 3) {
-      i = 0;
-      emitStaticColourAll(0, 0, 0, 0);
-      while (irq_flag == false) {
-        i = (direction == true) ? (i + 1) : (i - 1);
-        emitStaticColourAll(i, 0, 255 - i, 100);
-        delay(30);
-        if (i == 255) {
-          direction = false;
-        }
-        else if (i == 0) {
-          direction = true;
-        }
-      }
+      rainbowLights(brightness);
     }
 
     else if (mode == 4) {
-      i = 0;
-      emitStaticColourAll(0, 0, 0, 0);
-      while (irq_flag == false) {
-        i = (direction == true) ? (i + 1) : (i - 1);
-        emitStaticColourAll(r, g, b, i);
-        delay(10);
-        if (i == 255) {
-          direction = false;
-        }
-        else if (i == 0) {
-          direction = true;
-        }
-      }
+      fadingLights(r, g, b, brightness);
     }
 
     else if (mode == 5) {
-      i = 0;
-      emitStaticColourAll(0, 0, 0, 0);      
-      while (irq_flag == false) {
-        if (isToBeTurnedOn == true) {
-          emitStaticColour(r, g, b, 100, i);
-        }
-        else {
-          emitStaticColour(0, 0, 0, 100, i);
-        }
-        delay(100);
-        if (i == 20) {
-          isToBeTurnedOn = !(isToBeTurnedOn);
-          i = 0;
-        }
-        else {
-          i++;
-        }  
-        
-
-        }
-      }
+      patternLights(r, g, b, brightness);
+    }
   }
 }

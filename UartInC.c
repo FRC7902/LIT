1f674dff7652387aadd909976afbd63c60320848
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2024-10-12 03:47:09 -0400

    Update UartInC.c
    functional except for pattern lights and fading lights

--- UartInC.c
+++ UartInC.c
@@ -1,112 +1,163 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/interp.h"
 #include "hardware/uart.h"
-#include "ws2812/generated/ws2812.pio.h"
+#include "math.h"
+#include "hardware/clocks.h"
+#include "ws2812/ws2812.pio.h"
 
 #define UART_ID uart0
 #define BAUD_RATE 300
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
 #define WS2812_PIN 2
-#define NUM_PIXELS 20
-#define IS_RGBW false
 #define IRQ_PIN 3
 
-#define LED_DELAY_MS 2000;
+struct RGB{
+uint8_t r;
+uint8_t g;
+uint8_t b;
+};
 
-void pico_led_init(void) {
-  gpio_init(25);
-  gpio_set_dir(25, GPIO_OUT);
-}
-
-char mode, brightness, r, g, b, stringy[5];
+int mode, brightness, r, g, b, NUM_PIXELS = 20, stringy[5];
 bool irq_flag = false, direction = true, isToBeTurnedOn = true;
-int i, bytes_transmitted;
+int bytes_transmitted;
+float i;
+uint32_t colour_set;
 
 static inline void put_pixel(uint32_t pixel_grb) {
     pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
 }
 
 static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
-    return
-            ((uint32_t) (r) << 8) |
-            ((uint32_t) (g) << 16) |
-            (uint32_t) (b);
+  return ((uint32_t)(r) << 8) |
+         ((uint32_t)(g) << 16) |
+         (uint32_t)(b);
 }
 
-static inline uint32_t urgbw_u32(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
-    return
-            ((uint32_t) (r) << 8) |
-            ((uint32_t) (g) << 16) |
-            ((uint32_t) (w) << 24) |
-            (uint32_t) (b);
+void emitStaticColourAll(double r, double g, double b, double brightness) {
+      colour_set = urgb_u32((r), (g), (b));
+      for (int x = 0; x < NUM_PIXELS; x++) {
+        put_pixel(colour_set);
+      }
 }
 
-void emitStaticColourAll(int r, int g, int b, int brightness) {
-    put_pixel(urgb_u32(r, g, b));
-}
-
-void emitStaticColour(int r, int g, int b, int brightness, int ledIndex) {
-    put_pixel(urgb_u32(r, g, b));
+void emitStaticColour(double r, double g, double b, double brightness, int ledIndex) {
+    for (int g = ledIndex; g != 0 || g != 20; g--) {
+      put_pixel(colour_set);
+    }
+    colour_set = urgb_u32((r), (g), (b));
+    put_pixel(colour_set);
 }
 
 void flashingLights(int r, int g, int b, int brightness) {
-  emitStaticColourAll(0, 0, 0, 0);
   while (irq_flag == false) {
     emitStaticColourAll(r, g, b, brightness);
-    sleep_ms(500000);
+    sleep_ms(500);
     emitStaticColourAll(0, 0, 0, 0);
-    sleep_ms(500000);
+    sleep_ms(500);
   }
 }
 
-void rainbowLights(int brightness) {
-  i = 0;
-  emitStaticColourAll(0, 0, 0, 0);
+void rainbowLights(double r, double g, double b, int brightness) {
+  i = 0.2;
+  r = 255.0;
+  g = 255.0;
+  b = 255.0;
+  int step = 1, ranOnce = 0;
+  irq_flag = false;
+  direction = true;
   while (irq_flag == false) {
-    i = (direction == true) ? (i + 1) : (i - 1);
-    emitStaticColourAll(i, 0, 255 - i, brightness);
-    sleep_ms(30000);
-    if (i == 253) {
-      direction = false;
+    //i = (direction == true) ? (i + 0.02) : (i - 0.02);
+    i += 0.01;
+
+    if (step == 1) {
+      if (direction == true) {
+        r -= i;
+      }
+      else if (direction == false) {
+        r += i;
+      }
     }
-    else if (i == 3) {
-      direction = true;
+    else if (step == 2) {
+      if (direction == true) {
+        r += i;
+        g -= i;
+      }
+      else if (direction == false) {
+        r -= i;
+        g += i;
+      }
+    }
+
+    else if (step == 3) {
+      if (direction == true) {
+        g += i;
+        b -= i;
+      }
+      else if (direction == false) {
+        g -= i;
+        b += i;
+      }
+
+    }
+
+    sleep_ms(25);
+    emitStaticColourAll(r, g, b, brightness);
+
+    sprintf(stringy, "%d", step);
+    printf(stringy);
+    printf("\nR:\n\n");
+
+    if (i >= 3.4) {
+      if (step == 3 && direction == true) {
+        direction = false;
+        step++;
+      }
+      else if (step == 1 && direction == false) {
+        direction = true;
+        step--;
+      }
+      if (direction == false) {
+        i = 0.2;
+        step--;  
+      }
+      else if (direction == true) {
+        i = 0.2;
+        step++;
+      }       
     }
   }
 }
+
 
 void fadingLights(int r, int g, int b, int brightness) {
-  i = 0;
-  emitStaticColourAll(0, 0, 0, 0);
-  while (irq_flag == false) {
-    i = (direction == true) ? (i + 1) : (i - 1);
-    emitStaticColourAll(r, g, b, i);
-    sleep_ms(1000);
-    if (i == 255) {
+  direction = true;
+  for (i = 0; irq_flag != true;) {
+    i = (direction == true) ? (i + 0.01) : (i - 0.01);
+    r = (r == 0) ? (r) : (direction == true) ? (r - i) : (r + i);
+    g = (g == 0) ? (g) : (direction == true) ? (g - i) : (g + i);
+    b = (b == 0) ? (b) : (direction == true) ? (b - i) : (b + i);
+    emitStaticColourAll(r, g, b, 100);
+    if (i >= 1.7 && direction == true) {
       direction = false;
     }
-    else if (i == 0) {
+    else if (i == 0.01) {
       direction = true;
-    }
+    } 
+    sleep_ms(10);
   }
 }
 
 void patternLights(int r, int g, int b, int brightness) {
   i = 0;
-  emitStaticColourAll(0, 0, 0, 0);      
   while (irq_flag == false) {
-    if (isToBeTurnedOn == true) {
-      emitStaticColour(r, g, b, 100, i);
-    }
-    else {
-      emitStaticColour(0, 0, 0, 100, i);
-    }
-    sleep_ms(100000);
+    emitStaticColour(r, g, b, brightness, i);
+    sleep_ms(100);
     if (i == 20) {
       isToBeTurnedOn = !(isToBeTurnedOn);
       i = 0;
@@ -118,99 +169,104 @@ void patternLights(int r, int g, int b, int brightness) {
 }
 
 void interrupt() {
+    printf("\nYOU INTERRUPTED BITCH");
     irq_flag = true;
 }
 
-
 int main()
 {   
-    gpio_set_irq_enabled_with_callback(IRQ_PIN, GPIO_IRQ_EDGE_RISE, true, interrupt);
-
     stdio_init_all();
+    PIO pio = pio0;
+    int sm = 0;
+    uint offset = pio_add_program(pio, &ws2812_program);
+    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, false);
+
     gpio_set_dir(IRQ_PIN, GPIO_OUT);   
     gpio_pull_down(IRQ_PIN);
     uart_init(UART_ID, BAUD_RATE);
+    gpio_set_irq_enabled_with_callback(IRQ_PIN, GPIO_IRQ_EDGE_RISE, true, interrupt);
 
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-  
-    pico_led_init();
-    //gpio_put(25, true);
+
     while (true)
     {
         if (uart_is_readable(UART_ID))
         {
-            //gpio_put(25, true);
-            printf("yay");
+            //emitStaticColourAll(0, 0, 0, 0);
+            printf("yey\n");
             brightness = mode = r = g = b = 0;
-            // Serial.println(bytes_transmitted);
             irq_flag = false;
-            brightness = (bytes_transmitted >= 0) ? (uart_getc(UART_ID)) : (0);
-            //if (mode == 0) {
-              //gpio_put(25, true);
-            //}
-            mode = (bytes_transmitted >= 1) ? (uart_getc(UART_ID)) : (0);
-            r = (bytes_transmitted >= 2) ? (uart_getc(UART_ID)): (0);
-            g = (bytes_transmitted >= 3) ? (uart_getc(UART_ID)) : (0);
-            b = (bytes_transmitted >= 4) ? (uart_getc(UART_ID)) : (0);
 
-            sleep_ms(500);
+            brightness = uart_getc(UART_ID);
+            mode = uart_getc(UART_ID);
+          
+            r = uart_getc(UART_ID);
+            g = uart_getc(UART_ID);
+            b = uart_getc(UART_ID);
+
+            //mode = (bytes_transmitted >= 1) ? ((uart_getc(UART_ID)) / 10000) : (0);
+           // r = (bytes_transmitted >= 2) ? ((uart_getc(UART_ID)) / 10000): (0);
+            //g = (bytes_transmitted >= 3) ? ((uart_getc(UART_ID)) / 10000) : (0);
+            //b = (bytes_transmitted >= 4) ? ((uart_getc(UART_ID)) / 10000) : (0);
 
             sprintf(stringy, "%d", brightness);
             printf(stringy);
+            printf("\nBrightness:\n\n");
 
             sprintf(stringy, "%d", mode);
             printf(stringy);
+            printf("\nMode:\n\n");
 
             sprintf(stringy, "%d", r);
             printf(stringy);
+            printf("\nR:\n\n");
 
             sprintf(stringy, "%d", g);
             printf(stringy);
+            printf("\nG:\n\n");
 
             sprintf(stringy, "%d", b);
             printf(stringy);
-            
+            printf("\nB:\n\n");
+           
 
-            mode = 1;
             if (brightness == 0)
             {
+                irq_flag = false;
                 emitStaticColourAll(0, 0, 0, 0);
             }
 
             else if (mode == 1)
-            {
-                gpio_put(25, true);
-                emitStaticColourAll(0, 0, 0, 0);
-                emitStaticColourAll(255, 255, 255, 100);
+            { 
+                irq_flag = false;
+                emitStaticColourAll(r, g, b, 100);
             }
 
             else if (mode == 2)
             {
+                irq_flag = false;
                 flashingLights(r, g, b, brightness);
             }
 
             else if (mode == 3)
             {
-                rainbowLights(brightness);
+                irq_flag = false;
+                rainbowLights(r, g, b, brightness);
             }
 
             else if (mode == 4)
             {
+                irq_flag = false;
                 fadingLights(r, g, b, brightness);
             }
 
             else if (mode == 5)
             {
+                irq_flag = false;
                 patternLights(r, g, b, brightness);
             }
         }
     }
-        // Use some the various UART functions to send out data
-        // In a default system, printf will also output via the default UART
-
-        // Send out a string, with CR/LF conversions
-
-        // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
     
 }
